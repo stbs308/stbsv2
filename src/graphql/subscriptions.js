def1c9d796a2d48afadd03a8353253969914ce0f
Eric Hernandez <ehernand@us.ibm.com@Erics-MBP.home>
2021-08-15 07:21:27 -0500

    after published

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,111 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onCreateCalEvent = /* GraphQL */ `
+  subscription OnCreateCalEvent {
+    onCreateCalEvent {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onUpdateCalEvent = /* GraphQL */ `
+  subscription OnUpdateCalEvent {
+    onUpdateCalEvent {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const onDeleteCalEvent = /* GraphQL */ `
+  subscription OnDeleteCalEvent {
+    onDeleteCalEvent {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
