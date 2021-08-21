1ea687adb028b2adfeebc3480dc8fc902781792e
Eric Hernandez <ehernand@us.ibm.com@Erics-MBP.home>
2021-08-21 17:43:57 -0500

    final 1

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,6 +1,231 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const newOnCreateCalEvent = /* GraphQL */ `
+  subscription NewOnCreateCalEvent {
+    newOnCreateCalEvent {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const newOnUpdateCalEvent = /* GraphQL */ `
+  subscription NewOnUpdateCalEvent {
+    newOnUpdateCalEvent {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const newOnDeleteCalEvent = /* GraphQL */ `
+  subscription NewOnDeleteCalEvent {
+    newOnDeleteCalEvent {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const techOnCreateCalEvent = /* GraphQL */ `
+  subscription TechOnCreateCalEvent($category: String!) {
+    techOnCreateCalEvent(category: $category) {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const techOnUpdateCalEvent = /* GraphQL */ `
+  subscription TechOnUpdateCalEvent($category: String!) {
+    techOnUpdateCalEvent(category: $category) {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const techOnDeleteCalEvent = /* GraphQL */ `
+  subscription TechOnDeleteCalEvent($category: String!) {
+    techOnDeleteCalEvent(category: $category) {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const custOnCreateCalEvent = /* GraphQL */ `
+  subscription CustOnCreateCalEvent($owner2: String!) {
+    custOnCreateCalEvent(owner2: $owner2) {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const custOnUpdateCalEvent = /* GraphQL */ `
+  subscription CustOnUpdateCalEvent($owner2: String!) {
+    custOnUpdateCalEvent(owner2: $owner2) {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
+export const custOnDeleteCalEvent = /* GraphQL */ `
+  subscription CustOnDeleteCalEvent($owner2: String!) {
+    custOnDeleteCalEvent(owner2: $owner2) {
+      id
+      name
+      name2
+      details
+      start
+      end
+      time_of_day
+      admin_notes
+      emp_notes
+      category
+      color
+      apt_num
+      apt_status
+      service_category
+      owner2
+      note_code
+      createdAt
+      updatedAt
+      owner
+    }
+  }
+`;
 export const onCreateTodo = /* GraphQL */ `
   subscription OnCreateTodo {
     onCreateTodo {
