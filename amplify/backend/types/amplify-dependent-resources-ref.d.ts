8391415b579af11cca5302f429373d080011eeec
Eric Hernandez <erichernandez@Erics-MacBook-Pro.local>
2022-11-03 14:17:36 -0500

    before adding new dialog for new users

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,16 +1,4 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "stbsv284d59e72": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
-        }
-    },
     "api": {
         "stbsv2": {
             "GraphQLAPIIdOutput": "string",
