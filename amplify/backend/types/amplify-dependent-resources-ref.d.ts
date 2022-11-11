cd7905aee78347520d6f049432e8773ae39b3831
Eric Hernandez <erichernandez@Erics-MBP.home>
2022-11-10 21:54:10 -0600

    change employee to category

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,16 @@
 export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "stbsv284d59e72": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
     "api": {
         "stbsv2": {
             "GraphQLAPIIdOutput": "string",
